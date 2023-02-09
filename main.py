e80810ec68615d2d3f5cc67c6106b482a3201a17
Antoine <a.merz@student.helmo.be>
2023-02-09 20:15:44 +0100

    Modification

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import os
+import time
+
 from file import file_processing as file
 
 #Selection des chemins d'accès vers les fichiers et mise en variable des lites
@@ -6,6 +8,7 @@ files_path = file.get_files_path()
 content = file.extract_content(files_path)
 
 #TEST
+os.mkdir('temp')
 os.chdir('.\\temp')
 
 fw = open('temp.html','w',encoding='utf-8')
@@ -17,3 +20,7 @@ sw.writelines(content[1])
 
 fw.close()
 sw.close()
+os.remove('temp.html')
+os.remove('temp2.html')
+os.chdir('..\\')
+os.rmdir('temp')
