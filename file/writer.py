65a74433a6b4844396df042bb82a446b14cd3cb3
Antoine <a.merz@student.helmo.be>
2023-02-10 00:53:54 +0100

    Module d'écriture du nouveau preset

--- file/writer.py
+++ file/writer.py
@@ -5,7 +5,7 @@ from file import extractor as extract
 
 def write_new_preset(mods_sets):
     mods_list = list(mods_sets)
-    np = open(set_new_preset_path(),'w',encoding='utf-8')
+    np = open(set_new_preset_path()+'.html','w',encoding='utf-8')
     np.writelines(extract.extract_header())
     np.writelines(mods_list)
     np.writelines(extract.extract_footer())
