760d1a2f4bb54ec038ca04e2cef9645eaec02127
Antoine <a.merz@student.helmo.be>
2023-02-09 23:29:20 +0100

    Modification

--- file/extractor.py
+++ file/extractor.py
@@ -1,3 +1,10 @@
+from file import file_processing as file
+import os
+import re
+
+
+PATTERN = "<tr.*\\s*.*\\s*.*\\s.*\\s.*\\s.*\\s.*\\s.*\\s.*</tr>"
+
 from file.file_processing import file_to_list
 
 
@@ -8,4 +15,17 @@ def extract_content(files_path):
 
 
 def extract_mods():
-    return None
+    os.chdir('.//temp')
+    f1 = open('temp.html','r',encoding='utf-8')
+    f2 = open('temp2.html','r',encoding='utf-8')
+    lists = list_creation(f1.read(),f2.read())
+    f1.close()
+    f2.close()
+    os.chdir('..//')
+    file.delete_temp()
+    return lists
+
+def list_creation(html_temp,html_temp2):
+    liste_temp = re.findall(PATTERN, html_temp)
+    liste_temp2 = re.findall(PATTERN, html_temp2)
+    return set(liste_temp), set(liste_temp2)
--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 from file import file_processing as file
+from file import extractor as extractor
 
 files_path = file.get_files_path()
-content = file.extract_content(files_path)
+content = extractor.extract_content(files_path)
+file.create_temp(content)
+lists_mods = extractor.extract_mods()
 
-
+#différence
+mods_difference = lists_mods[0] ^ lists_mods[1]
+print(mods_difference)
